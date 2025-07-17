8689169287a53c52246c7e5c784b7e921c0a17d4
LikeJuggger <kudriavtsevdu@gmail.com>
2025-07-18 00:58:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -163,7 +163,7 @@ async def main():
     app.add_handler(conv)
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_reminders, trigger='cron', hour=21, minute=30, args=[app.bot])
+    scheduler.add_job(send_reminders, trigger='cron', hour=22, minute=00, args=[app.bot])
     scheduler.start()
 
     print("🤖 Бот запущено!")
